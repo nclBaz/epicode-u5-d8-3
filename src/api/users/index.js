ce6781208686b4ea26b02676a7ec2c7c1706b9f8
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-02-08 11:47:18 +0100

    Adds JWT Middleware

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import createHttpError from "http-errors"
 import { adminOnlyMiddleware } from "../../lib/auth/adminOnly.js"
-import { basicAuthMiddleware } from "../../lib/auth/basicAuth.js"
+import { JWTAuthMiddleware } from "../../lib/auth/jwtAuth.js"
 import { createAccessToken } from "../../lib/auth/tools.js"
 import UsersModel from "./model.js"
 
@@ -17,7 +17,7 @@ usersRouter.post("/", async (req, res, next) => {
   }
 })
 
-usersRouter.get("/", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
+usersRouter.get("/", JWTAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
   try {
     const users = await UsersModel.find({})
     res.send(users)
@@ -26,15 +26,16 @@ usersRouter.get("/", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, 
   }
 })
 
-usersRouter.get("/me", basicAuthMiddleware, async (req, res, next) => {
+usersRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
   try {
-    res.send(req.user)
+    const user = await UsersModel.findById(req.user._id)
+    res.send(user)
   } catch (error) {
     next(error)
   }
 })
 
-usersRouter.put("/me", basicAuthMiddleware, async (req, res, next) => {
+usersRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const updatedUser = await UsersModel.findByIdAndUpdate(req.user._id, req.body, {
       new: true,
@@ -46,7 +47,7 @@ usersRouter.put("/me", basicAuthMiddleware, async (req, res, next) => {
   }
 })
 
-usersRouter.delete("/me", basicAuthMiddleware, async (req, res, next) => {
+usersRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
   try {
     await UsersModel.findByIdAndUpdate(req.user._id)
     res.status(204).send()
@@ -55,7 +56,7 @@ usersRouter.delete("/me", basicAuthMiddleware, async (req, res, next) => {
   }
 })
 
-usersRouter.get("/:userId", basicAuthMiddleware, async (req, res, next) => {
+usersRouter.get("/:userId", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const user = await UsersModel.findById(req.params.userId)
     res.send(user)
@@ -63,14 +64,14 @@ usersRouter.get("/:userId", basicAuthMiddleware, async (req, res, next) => {
     next(error)
   }
 })
-usersRouter.put("/:userId", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
+usersRouter.put("/:userId", JWTAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
   try {
   } catch (error) {
     next(error)
   }
 })
 
-usersRouter.delete("/:userId", basicAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
+usersRouter.delete("/:userId", JWTAuthMiddleware, adminOnlyMiddleware, async (req, res, next) => {
   try {
   } catch (error) {
     next(error)
